748324f2bf41991cb2e3853381c77bfda10c7e78
Richard <richwats@cisco.com>
2022-08-25 12:37:09 +1000

    file acl

--- main.tf
+++ main.tf
@@ -55,3 +55,22 @@ module "acls" {
   content   = each.value.content
   switches  = each.value.switches
 }
+
+
+## Example 3 - Single ACL read from text file ##
+
+module "file-acl" {
+  source = "github.com/cisco-apjc-cloud-se/terraform-dcnm-freeform"
+
+  content = file("example_acl.txt")
+  switches  = {
+    DC1-LEAF-1 = {
+      name = "DC1-LEAF-1"
+      fabric = "CML2-DC1"
+    }
+    DC1-LEAF-2 = {
+      name = "DC1-LEAF-2"
+      fabric = "CML2-DC1"
+    }
+  }
+}
